b5193606497e9586eeeb5eea1f7423c39275d762
theheroproject <thenaheroproject@gmail.com>
2020-07-07 01:53:10 +0900

    20200706

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var ibm_db = require('ibm_db');
 var setting = require('./setting');
 
-let db_str = "DRIVER={DB2}"
-+ ";DATABASE=" + setting.DB_DATABASE
-+ ";HOSTNAME=" + setting.DB_HOSTNAME
-+ ";PORT=" + setting.DB_PORT
-+ ";PROTOCOL=TCPIP"
-+ ";UID=" + setting.DB_UID
-+ ";PWD=" + setting.DB_PWD;
+let db_str = "DRIVER={DB2}" +
+    ";DATABASE=" + setting.DB_DATABASE +
+    ";HOSTNAME=" + setting.DB_HOSTNAME +
+    ";PORT=" + setting.DB_PORT +
+    ";PROTOCOL=TCPIP" +
+    ";UID=" + setting.DB_UID +
+    ";PWD=" + setting.DB_PWD;
 //let sql = "select * from DB2WML.HOME_SALES";
-let sql = "select * from DB2WML.HOME_SALES WHERE ID = 1462";
+let sql = "select * from DB2WML.HOME_SALES ";
 //db2 'select * from DB2WML.HOME_SALES WHERE ID = 1461';
-//process.env.DB2CODEPAGE = 1252 ;
-//process.env.DB2CODEPAGE = 1208 ;
-process.env.DB2CODEPAGE = 1252 ;
+process.env.DB2CODEPAGE = 1252;
+//process.env.DB2CODEPAGE = 1208;
+//process.env.DB2CODEPAGE = 819;
+
 ibm_db.open(db_str, function(err, conn) {
     if (err) return console.log(err);
 
